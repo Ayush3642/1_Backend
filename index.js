60a66197fb334c36c0e4f1c746fe0946c625bad2
Ayush Abhay <abhayayush24@gmail.com>
2025-04-04 01:08:26 +0530

    add basic bakend App

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express"); // same as : import express from "express";
+require("dotenv").config(); // or import 'dotenv/config'
+
+const app = express();
+
+// const port = 4000; // 65,535 virtual ports in a system.
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("Hello Twitter!");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please Login to my site</h1");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
